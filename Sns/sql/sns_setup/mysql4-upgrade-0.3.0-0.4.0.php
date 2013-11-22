a84d39c897e0f0c31899f3123bbb13654d6774f7
root <root@AY1306100114420217d6Z.(none)>
2013-11-22 23:03:55 +0800

    add sql files for customer attributes

---
+++ Sns/sql/sns_setup/mysql4-upgrade-0.3.0-0.4.0.php
@@ -0,0 +1,51 @@
+<?php
+$installer = new Mage_Customer_Model_Entity_Setup;
+$installer->startSetup();
+
+$installer->addAttribute(
+    'customer',
+    'weibo_id',
+    array(
+        'group'                => 'Default',
+        'type'                 => 'varchar',
+        'label'                => 'Weibo Id',
+        'input'                => 'text',
+        'source'               => 'eav/entity_attribute_source_text',
+        'global'               => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+        'required'             => 0,
+        'default'              => 0,
+        'visible'              => 0,
+        'adminhtml_only'       => 1,
+        'user_defined'  => 1,
+    )
+);
+
+$oAttribute = Mage::getSingleton('eav/config')->getAttribute('customer', 'weibo_id');
+$oAttribute->setData('used_in_forms', array('adminhtml_customer'));
+$oAttribute->save();
+$installer->endSetup();
+$installer = new Mage_Customer_Model_Entity_Setup;
+$installer->startSetup();
+
+$installer->addAttribute(
+    'customer',
+    'qq_id',
+    array(
+        'group'                => 'Default',
+        'type'                 => 'varchar',
+        'label'                => 'QQ Id',
+        'input'                => 'text',
+        'source'               => 'eav/entity_attribute_source_text',
+        'global'               => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+        'required'             => 0,
+        'default'              => 0,
+        'visible'              => 0,
+        'adminhtml_only'       => 1,
+        'user_defined'  => 1,
+    )
+);
+
+$oAttribute = Mage::getSingleton('eav/config')->getAttribute('customer', 'qq_id');
+$oAttribute->setData('used_in_forms', array('adminhtml_customer'));
+$oAttribute->save();
+$installer->endSetup();
